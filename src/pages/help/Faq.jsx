e51ec277c8767f5cb3634715a3887857bc7eff5d
Hebbar <prajwalhebbaras@gmail.com>
2024-06-07 17:49:43 +0530

    Added styling and content for faq layout in inside help

--- src/pages/help/Faq.jsx
+++ src/pages/help/Faq.jsx
@@ -1,4 +1,62 @@
 const Faq = () => {
-  return <div>faq</div>;
+  return (
+    <div className="faq">
+      <h3>Frequently Asked Questions</h3>
+      <div className="question">
+        <p>
+          <strong>How do I create an account on JobScout?</strong>
+        </p>
+        <p>
+          To create an account, click on the "Sign Up" button on the top right
+          corner of the homepage. Fill in the required details, and you’ll be
+          all set to start exploring job opportunities.
+        </p>
+      </div>
+      <div className="question">
+        <p>
+          <strong> How do personalized job recommendations work?</strong>
+        </p>
+        <p>
+          To create an account, click on the "Sign Up" button on the top right
+          corner of the homepage. Fill in the required details, and you’ll be
+          all set to start exploring job opportunities.
+        </p>
+      </div>
+      <div className="question">
+        <p>
+          <strong>How can I track my job applications?</strong>
+        </p>
+        <p>
+          Once you apply for a job through JobScout, you can track the status of
+          your applications in the "My Applications" section of your dashboard.
+          Here you can see which stage each application is at, from submission
+          to interview scheduling and final decisions.
+        </p>
+      </div>
+      <div className="question">
+        <p>
+          <strong>
+            What resources does JobScout offer for career development?
+          </strong>
+        </p>
+        <p>
+          JobScout provides a wealth of resources including resume building
+          tips, interview preparation guides, career advice articles, and
+          industry insights. You can access these resources in the "Career
+          Resources" section.
+        </p>
+      </div>
+      <div className="question">
+        <p>
+          <strong>How do I update my profile information?</strong>
+        </p>
+        <p>
+          To update your profile information, go to your dashboard and click on
+          the "Profile" tab. Here you can edit your personal details, skills,
+          experience, and preferences.
+        </p>
+      </div>
+    </div>
+  );
 };
 export default Faq;
