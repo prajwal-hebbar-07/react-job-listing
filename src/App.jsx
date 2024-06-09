458c54f3c22095e58d7e2b9b61d9d5e44dbb8568
Hebbar <prajwalhebbaras@gmail.com>
2024-06-09 08:23:49 +0530

    Contact page implementation complete with form input

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import HelpLayout from "./layouts/HelpLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Faq from "./pages/help/Faq";
-import Contact from "./pages/help/Contact";
+import Contact, { contactAction } from "./pages/help/Contact";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,7 +22,7 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="contact" element={<Contact />} action={contactAction} />
       </Route>
     </Route>
   )
--- src/pages/help/Contact.jsx
+++ src/pages/help/Contact.jsx
@@ -1,4 +1,49 @@
+import { Form, useActionData } from "react-router-dom";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const data = useActionData();
+
+  return (
+    <div className="contact-us">
+      <h3>Contact Us</h3>
+      <p>
+        If you have any questions, concerns, or feedback, we’re here to help!
+        Please use the form below to get in touch with us, and one of our
+        support team members will get back to you as soon as possible.
+      </p>
+      <Form method="post" action="/help/contact">
+        <label>
+          <span>Email: </span>
+          <input type="email" name="email" required />
+        </label>
+        <label>
+          <span>Message: </span>
+          <textarea type="text" name="message" required></textarea>
+        </label>
+        <button>Submit</button>
+
+        {data && <p>{data}</p>}
+      </Form>
+    </div>
+  );
 };
 export default Contact;
+
+export const contactAction = async ({ request }) => {
+  const data = await request.formData();
+  const email = data.get("email");
+  const message = data.get("message");
+  const submissionData = {
+    email,
+    message,
+  };
+  console.log(
+    `Received message from ${submissionData.email}: ${submissionData.message}`
+  );
+
+  if (submissionData.message.length < 10) {
+    return new Response("Message is too short. Please provide more details.");
+  }
+
+  return new Response("Message received. We'll get back to you soon!");
+};
