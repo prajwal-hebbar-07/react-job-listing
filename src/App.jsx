9861bd39a95ee59c925c4a9a8896f1aa4d0fff3d
Hebbar <prajwalhebbaras@gmail.com>
2024-06-10 11:58:54 +0530

    Careers Page error element added and styled

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,12 @@ import About from "./pages/About";
 import Faq from "./pages/help/Faq";
 import Contact, { contactAction } from "./pages/help/Contact";
 import Careers, { careersLoader } from "./pages/careers/Careers";
+import CareerDetails, {
+  careerDetailsLoader,
+} from "./pages/careers/CareerDetails";
+
+// Error Layouts (or) Error Pages
+import CareersError from "./pages/careers/CareersError";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,8 +32,17 @@ const router = createBrowserRouter(
         <Route path="faq" element={<Faq />} />
         <Route path="contact" element={<Contact />} action={contactAction} />
       </Route>
-      <Route path="careers" element={<CareersLayout />}>
+      <Route
+        path="careers"
+        element={<CareersLayout />}
+        errorElement={<CareersError />}
+      >
         <Route index element={<Careers />} loader={careersLoader} />
+        <Route
+          path=":id"
+          element={<CareerDetails />}
+          loader={careerDetailsLoader}
+        />
       </Route>
     </Route>
   )
---
+++ src/pages/careers/CareersError.jsx
@@ -0,0 +1,14 @@
+import { Link, useRouteError } from "react-router-dom";
+
+const CareersError = () => {
+  const error = useRouteError();
+
+  return (
+    <div className="careers-error">
+      <h2>Error</h2>
+      <p>{error.message}</p>
+      <Link to="/">Back to Home</Link>
+    </div>
+  );
+};
+export default CareersError;
